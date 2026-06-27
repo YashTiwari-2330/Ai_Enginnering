e9ed63d74955eb0b0a76dc80370f9ab6929565a8
YashTiwari-2330 <yashtiwari2330@gmail.com>
2026-06-27 12:58:58 +0530

    About the query parameters with an examples

--- Phase1-BackendEng/Project/main.py
+++ Phase1-BackendEng/Project/main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path , HTTPException, Query
 import json
-from pathlib import Path
+from pathlib import Path as FilePath
 
 app = FastAPI()
 
 # Lode the file and data
 def load_data():
-    file_path = Path(__file__).with_name("patients.json")
+    file_path = FilePath(__file__).with_name("patients.json")
     with open(file_path , 'r') as f:
         data = json.load(f)
     return data
@@ -32,11 +32,30 @@ def show_patients():
     return "Patients List : " + str(data)
 
 @app.get('/patients/{patient_id}')
-def show_patient(patient_id : str):
+def show_patient(patient_id : str = Path(..., description="The ID of the patient to retrieve" , example = "P001")):
     data = load_data()
     for patient in data:
         if patient["id"] == patient_id:
             return patient
 
-    return "Patient not found."
+    raise HTTPException(status_code=404 , detail=f"Patient With ID {patient_id} not found..")
     
+# Sort data
+@app.get('/sort')
+def sort_patients(sort_by : str = Query(..., description="The field to sort the patients basis of hight , weight or bmi"), order : str = Query('asc' , description="Sort in asc and dasc order")):
+
+    valid_fields = ["height" , "weight" , "bmi"]
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400 , detail=f"Enter a valid field from {valid_fields}")
+    
+    if order not in ['asc' , 'desc']:
+        raise HTTPException(status_code=400 , detail= f"Enter a valid order from ['asc'] or ['desc'] ")
+    
+    data = load_data()
+
+    sorted_data = sorted(data, key = lambda x : x.get(sort_by) , reverse = (order == 'desc'))
+
+    return sorted_data
+    
+
